94eb237d30e9a3a6645c8f9eeedede9942b971f9
FeralSmurf <bogdanvarzaru@gmail.com>
2024-04-12 19:57:31 +0300

    with copilot and copilot chat

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,4 +14,52 @@ return {
       }
     end,
   },
+
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- default mappings
+    mappings = {
+      complete = {
+        detail = 'Use @<Tab> or /<Tab> for options.',
+        insert = '<Tab>',
+      },
+      close = {
+        normal = 'q',
+        insert = '<C-c>',
+      },
+      reset = {
+        normal = '<C-l>',
+        insert = '<C-l>',
+      },
+      submit_prompt = {
+        normal = '<CR>',
+        insert = '<C-m>',
+      },
+      accept_diff = {
+        normal = '<C-S-y>',
+        insert = '<C-S-y>',
+      },
+      yank_diff = {
+        normal = 'gy',
+      },
+      show_diff = {
+        normal = 'gd',
+      },
+      show_system_prompt = {
+        normal = 'gp',
+      },
+      show_user_selection = {
+        normal = 'gs',
+      },
+    },
+  }, -- See Commands section for default commands if you want to lazy load on them
 }
